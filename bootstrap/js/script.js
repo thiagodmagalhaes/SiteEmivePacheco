6c661754ea112420edf32779d6a3baff88bcd0e5
Thiago Magalhães <thiago.dmagalhaes@hotmail.com>
2024-08-04 16:42:12 -0300

    Versão 2 - SitePacheco

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -1,54 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const secServSection = document.getElementById('secServ');
-  const showSmartSection = document.querySelector('.showSmart');
-  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-  const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl, {
+            trigger: 'hover focus' // Exibe o popover ao passar o mouse ou focar a imagem
+        });
+    });
 
+    const carouselElement = document.querySelector('#carouselExampleIndicators');
+    const carousel = new bootstrap.Carousel(carouselElement, {
+        interval: 10000,
+        ride: 'carousel'
+    });
+});
 
-  // Função para verificar a visibilidade da seção
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+document.getElementById('orcamento-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
 
-  // Função para lidar com a visibilidade e acionar o ScrollReveal
-  function handleVisibility() {
-    if (isElementInViewport(showSmartSection)) {
-      // Adiciona a classe 'visible' à seção secServ quando a seção showSmart estiver visível
-      secServSection.classList.add('visible');
+    // Cria uma solicitação AJAX para enviar o formulário
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', this.action, true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-      // Inicializa o ScrollReveal apenas quando a seção showSmart estiver visível
-      ScrollReveal({ duration: 1000 });
-      ScrollReveal().reveal('.headline');
-      ScrollReveal().reveal('.tagline', { delay: 500 });
-      ScrollReveal().reveal('.tagline2', { delay: 600 });
-      ScrollReveal().reveal('.tagline3', { delay: 700 });
-      ScrollReveal().reveal('.tagline4', { delay: 800 });
-      ScrollReveal().reveal('.tagline5', { delay: 900 });
-      ScrollReveal().reveal('.tagline5', { delay: 1000 });
-      ScrollReveal().reveal('.tagline6', { delay: 1100 });
-      ScrollReveal().reveal('.tagline7', { delay: 1150 });
-      ScrollReveal().reveal('.tagline8', { delay: 1300 });
-      ScrollReveal().reveal('.tagline9', { delay: 1400 });
-      ScrollReveal().reveal('.tagline10', { delay: 1500 });
-      ScrollReveal().reveal('.tagline11', { delay: 1600 });
-      ScrollReveal().reveal('.tagline12', { delay: 1700 });
-      ScrollReveal().reveal('.tagline13', { delay: 1800 });
+    // Cria um objeto de dados do formulário
+    var formData = new FormData(this);
+    var encodedData = new URLSearchParams(formData).toString();
 
+    // Define a ação para após a solicitação ser concluída
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                console.log('Formulário enviado com sucesso!');
+                window.location.href = 'obrigado.html'; // Redireciona para a página de agradecimento
+            } else {
+                console.error('Erro ao enviar o formulário: ', xhr.status, xhr.statusText);
+                alert('Ocorreu um erro. Por favor, tente novamente.');
+            }
+        }
+    };
 
-      // Remove o listener após a ativação para não reativar o ScrollReveal
-      window.removeEventListener('scroll', handleVisibility);
-    }
-  }
-
-  // Adiciona um listener de rolagem para verificar a visibilidade
-  window.addEventListener('scroll', handleVisibility);
-
-  // Verifica a visibilidade ao carregar a página
-  handleVisibility();
+    // Envia os dados do formulário
+    xhr.send(encodedData);
 });
